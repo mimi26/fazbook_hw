c0765325e2a01ec334cc30202e87fb20d21ad40c
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-01-26 19:23:27 -0500

    Add user create functionality and display users index page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../server/models/index');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users/index', { title: 'fazbook' });
+  models.User.findAll({}).then(function(users) {
+    res.render('users/index', {
+      title: 'fazbook',
+      users: users
+    });
+  });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('users/new', {
+    title: 'new user'
+  });
+});
+
+router.post('/', function(req, res, next) {
+  models.User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    dob: req.body.dob
+  }).then(function() {
+    res.redirect('/users')
+  });
 });
 
 module.exports = router;
