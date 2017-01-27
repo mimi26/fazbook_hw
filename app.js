9be3f3edeb8041b9ac31648953c98dc98fbeeb8d
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-01-26 19:53:26 -0500

    Add ability to delete users

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+//override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/users.js
+++ routes/users.js
@@ -29,4 +29,12 @@ router.post('/', function(req, res, next) {
   });
 });
 
+router.delete('/:id', function(req, res, next) {
+  models.User.destroy({
+    where: { id: req.params.id }
+  }).then(function(user) {
+    res.redirect('/users');
+  });
+});
+
 module.exports = router;
